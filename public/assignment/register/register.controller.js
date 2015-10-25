aa4359a54feb322eaf6d57d98e02025d9ae9ecad
Sagar Neeli <sagarneeli1191@gmail.com>
2015-10-25 13:28:39 -0400

    Fixed the register controller

--- public/assignment/register/register.controller.js
+++ public/assignment/register/register.controller.js
@@ -3,6 +3,24 @@
 
     angular
         .module("FormBuilderApp")
+        .directive("compareTo", [function () {
+            return {
+                require: "ngModel",
+                scope: {
+                    otherModelValue: "=compareTo"
+                },
+                link: function(scope, element, attributes, ngModel) {
+
+                    ngModel.$validators.compareTo = function(modelValue) {
+                        return modelValue == scope.otherModelValue;
+                    };
+
+                    scope.$watch("otherModelValue", function() {
+                        ngModel.$validate();
+                    });
+                }
+            };
+        }])
         .controller("RegisterController", RegisterController);
 
     function RegisterController(UserService, $location, $rootScope, $scope)
