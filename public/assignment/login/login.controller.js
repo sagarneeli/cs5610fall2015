268b850cb8821e694e5388c98d8d4dce49db3c92
Sagar Neeli <sagarneeli1191@gmail.com>
2015-10-25 12:06:23 -0400

    added form validation for the register form

--- public/assignment/login/login.controller.js
+++ public/assignment/login/login.controller.js
@@ -16,11 +16,6 @@
             console.log("Given username " + $scope.user.username + " Given password " + $scope.user.password);
             UserService.findUserByUsernameAndPassword($scope.user.username, $scope.user.password, function (user) {
                 if (user != null) {
-                    //user = {
-                    //    username : $scope.user.username,
-                    //    password : $scope.user.password
-                    //};
-                    console.log("Current User " + user);
                     $rootScope.loggedInUser = user;
                     $rootScope.$broadcast('auth', user);
                     $location.path('/profile');
--- public/assignment/register/register.controller.js
+++ public/assignment/register/register.controller.js
@@ -10,21 +10,23 @@
         $scope.$location = $location;
 
         $scope.register = function register() {
-            var newUser = {
-                username : $scope.user.username,
-                password : $scope.user.password,
-                email : $scope.user.email
-            };
+            if ($scope.userForm.$valid) {
+                var newUser = {
+                    username : $scope.user.username,
+                    password : $scope.user.password,
+                    email : $scope.user.email
+                };
 
-            UserService.createUser(newUser, function (user) {
-                //console.log("New User " + user);
-                if (user == null) {
-                    return;
-                }
-                $rootScope.loggedInUser = user;
-                $rootScope.$broadcast('auth', user);
-                $location.path('/profile');
-            });
+                UserService.createUser(newUser, function (user) {
+                    //console.log("New User " + user);
+                    if (user == null) {
+                        return;
+                    }
+                    $rootScope.loggedInUser = user;
+                    $rootScope.$broadcast('auth', user);
+                    $location.path('/profile');
+                });
+            }
         };
     }
 })();
